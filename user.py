7b50641237e993477dee2fcd94a408ae0f8177fb
jakesIII <jayruma@yahoo.com>
2019-11-10 17:03:16 +0300

    first initial commit

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    ''' class that generates a new instance of users '''
+
+    def __init__(self, username, password):
+
+        '''define object properties '''
+
+        self.username = username
+        self.password = password
+        
