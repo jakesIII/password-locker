07ece3f0fe322b64535763100b58c0eade98dff5
jakesIII <jayruma@yahoo.com>
2019-11-11 06:29:16 +0300

    create a credentials class with methods

--- app-test.py
+++ app-test.py
@@ -1,5 +1,6 @@
 import unittest
 from user import User
+from credentials import Credentials
 
 class TestUser(unittest.TestCase):
     '''defines test cases for user methods'''
---
+++ credentials.py
@@ -0,0 +1,31 @@
+import random
+import pyperclip
+import string
+
+
+class Credentials:
+    '''class to instanciate new account credentials'''
+
+    generated = []
+
+    def __init__(self, app, password):
+        self.app = app
+        self.acc_name = acc_name
+        self.acc_password = acc_password
+
+    def save_data(self):
+        '''Method that saves our accounts'''
+        Credentials.generated.append(self)
+
+    def delete_data(self):
+        '''Method to delete an account of our list'''
+        
+        Credentials.generated.remove(self)
+
+    def random_password(self):
+        '''method to generate a random password'''
+
+        characters = string.ascii_lowercase + string.digits
+        gen_password = ''.join(random.choice(char) for i in range (0,9))
+
+        return random_password
