c46af17f1e96b3fae0a0c1b6676461cbefa7fa57
jakesIII <jayruma@yahoo.com>
2019-11-12 03:53:25 +0300

    resolved short code interaction bugs

--- run.py
+++ run.py
@@ -49,107 +49,122 @@ def pass_generate(length):
     return Credentials.random_password(length)
 
 def main():
-    print("This is password locker! To sign up, key 'su', to log in, key 'li' ...")
-    answer = input ('>>> ')
 
-    if answer = "li":
+    print("This is password locker!")
+    print("To sign up, key 'su', to log in, key 'li' and 'xx' to exit ...")
+    log_in = False
 
-        print("Enter a Username: ")
-        username = input('Enter username: ')
+    while True:
+        answer = input ('>>> ')
 
-        print("Enter a password: ")
-        password = input ('Enter password: ')
+        if answer == "li":
 
-        log_in = authenticate_user(username, password) if answer == 'li' else False
+            print("Enter your Username: ")
+            username = input('>>> ')
 
-        while log_in:
-            print("Use the following short codes to navigate \n ad - to save an existing account data \n cd - to create new account data \n vd - to view list of inputed data \n dd - to delete account data from list \n ex - to exit application")
-            short_code = input(">>>")
+            print("Enter your password: ")
+            password = input ('>>> ')
 
-            if short_code == "ad":
-                print ("Enter existing account data...")
+            log_in = authenticate_user(username, password) if answer == 'li' else False
+            print("Login successful!")
+            print('\n')
 
-                print ("Enter the name of the account")
-                app_title = input(">>>")
+            while log_in:
+                print("Use the following short codes to navigate \n ad - to save an existing account data \n cd - to create new account data \n vd - to view list of inputed data \n dd - to delete account data from list \n ex - to exit application")
+                short_code = input(">>>")
 
-                print ("Enter the username to the account")
-                acc_name = input (">>>")
+                if short_code == "ad":
+                    print ("Enter existing account data...")
 
-                print ("Enter the account's password")
-                acc_password = input (">>>")
+                    print ("Enter the name of the account")
+                    app_title = input(">>>")
 
-                save_data(users_data(app_title, acc_name, acc_password))
+                    print ("Enter the username to the account")
+                    acc_name = input (">>>")
 
-                print (f"Data for {app_title} has been saved.")
-
-            elif short_code == "cd":
-                print ("Create new account data...")
-
-                print ("Enter the name of the account")
-                app_title = input(">>>")
-
-                print ("Enter the username to the account")
-                acc_name = input (">>>")
-
-                print ("For the password; \n key in 'y' if you want a generated password \n 'n' if you want to input a custom one ")
-                answer_password = input(">>>")
-
-                if answer_password == "y":
-                    print("Input password preffered length...")
-                    pass_length= int(input(">>>"))
-                    gener_password = pass_generate(pass_length)
-                    print(f"{app_title}'s password is {gener_password}")
-
-                elif answer_password == "n":
-                    print("Input your own password")
+                    print ("Enter the account's password")
                     acc_password = input (">>>")
 
-                else:
-                    print ("Your choice is in valid, try again")
+                    save_user_data(users_data(app_title, acc_name, acc_password))
 
-                save_data(users_data(app_title, acc_name, acc_password))
+                    print (f"Data for {app_title} has been saved.")
 
-                print (f"Data for {app_title} has been saved.")
-                print('\n')
+                elif short_code == "cd":
+                    print ("Create new account data...")
 
-            elif short_code == "vd":
-                    if display_data():
-                        print("Here is a list of all your stored account data")
-                        print('\n')
+                    print ("Enter the name of the account")
+                    app_title = input(">>>")
 
-                        for data in display_data():
-                            print (f"Application Title >>> {data.app_title}")
-                            print (f"Application Username >>> {data.acc_name}")
-                            print (f"Application Password >>> {data.acc_password}")
+                    print ("Enter the username to the account")
+                    acc_name = input (">>>")
+
+                    print ("For the password; \n key in 'y' if you want a generated password \n 'n' if you want to input a custom one ")
+                    answer_password = input(">>>")
+
+                    if answer_password == "y":
+                        print("Input password preffered length...")
+                        pass_length= int(input(">>>"))
+                        acc_password = pass_generate(pass_length)
+                        print(f"{app_title}'s password is {acc_password}")
+
+                    elif answer_password == "n":
+                        print("Input your own password")
+                        acc_password = input (">>>")
 
                     else:
-                        print("You don't seem to have any stored account data")
-                        print('\n')
+                        print ("Your choice is in valid, try again")
 
-            elif short_code == "dd":
-                pass
+                    save_user_data(users_data(app_title, acc_name, acc_password))
 
-            elif short_code == "ex":
-                print("Exiting the application...Goodbye!")
-                exit()
+                    print (f"Data for {app_title} has been saved.")
+                    print('\n')
+
+                elif short_code == "vd":
+                        if display_data():
+                            print("Here is a list of all your stored account data")
+                            print('\n')
+
+                            for data in display_data():
+                                print (f"Application Title >>> {data.app_title}")
+                                print (f"Application Username >>> {data.acc_name}")
+                                print (f"Application Password >>> {data.acc_password}")
+
+                        else:
+                            print("You don't seem to have any stored account data")
+                            print('\n')
+
+                elif short_code == "dd":
+                    pass
+
+                elif short_code == "ex":
+                    print("Exiting the application...Goodbye!")
+                    exit()
+
+                else:
+                    print("Invalid code, try again")
 
             else:
-                print("Invalid code, try again")
+                print("Wrong username and/or password. Try again")
 
+        elif answer == "su":
+            print("Create a new account to store your data...")
+
+            print("Enter a Username:")
+            username = input(">>>")
+
+            print("Enter a password:")
+            password = input(">>>")
+
+            save_created_user(create_user(username, password))
+            print(f"Welcome {username}, your account has been successfully set up")
+            print('\n')
+            print("Now to login, use li")
+
+        elif answer == "xx":
+            print ("Goodbye!")
+            exit()
         else:
-            print("Wrong username and/or password. Try again")
+            print("Invalid code, try again")
 
-    elif answer == "su":
-        print("Create a new account to store your data...")
-
-        print("Enter a Username:")
-        username = Input(">>>")
-
-        print("Enter a password:")
-        password = input(">>>")
-
-    else:
-        print("Invalid code, try again")
-        
 if __name__ == '__main__':
     main()
